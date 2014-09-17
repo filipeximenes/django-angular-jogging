c9386ad6717c705587b28a858400d50707e99a6b
Filipe Ximenes <filipeximenes@gmail.com>
2014-09-16 23:40:42 -0300

    adding simple view loading angularjs

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', include('core.urls')),
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
 )
