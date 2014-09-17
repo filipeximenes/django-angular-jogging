b023d489760812d9a5a5e9d9ad51f3469e9db5c1
Filipe Ximenes <filipeximenes@gmail.com>
2014-09-16 23:20:47 -0300

    adding django-bower settings

--- jogging/settings.py
+++ jogging/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'djangobower',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +83,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+BOWER_INSTALLED_APPS = (
+    'angular#1.2.25',
+)
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'core/static')
