ef01cc9725e2db148752f00fd19d3235481454b4
Filipe Ximenes <filipeximenes@gmail.com>
2014-09-22 20:27:59 -0300

    creating timings model

---
+++ timings/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Timings',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('time', models.TimeField()),
+                ('distance', models.DecimalField(max_digits=5, decimal_places=0)),
+                ('date', models.DateField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- timings/models.py
+++ timings/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# coding: utf-8
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Timings(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    time = models.TimeField()
+    distance = models.DecimalField(max_digits=5, decimal_places=0)
+    date = models.DateField()
