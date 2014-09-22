80a34159dc75627578fee5fb4259af5e09540356
Filipe Ximenes <filipeximenes@gmail.com>
2014-09-22 20:47:23 -0300

    adding timings serializer to timings list endpoint

--- timings/endpoints.py
+++ timings/endpoints.py
@@ -3,7 +3,9 @@
 from rest_framework import generics
 
 from timings.models import Timings
+from timings.serializers import TimingsSerializer
 
 
 class TimingsListCreateEndpoint(generics.ListCreateAPIView):
     model = Timings
+    serializer_class = TimingsSerializer
